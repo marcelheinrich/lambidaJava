37c16a57b6b67055370604934dcea4c080a11ceb
Marcel Heinrich <marcellorenzo@gmail.com>
2020-12-04 10:06:13 -0300

    Integração com DynamoDB

--- src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
+++ src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
@@ -1,16 +1,23 @@
 package com.amazonaws.lambda.demo;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Date;
 import com.google.gson.Gson;
-import java.util.UUID;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 
+import java.util.Map;
+import java.util.UUID;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.PutItemSpec;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.s3.AmazonS3;
@@ -25,8 +32,14 @@ public class LambdaFunctionHandler implements RequestHandler<Object, MyLambdaRes
     	Gson gson = new Gson();
     	String json = gson.toJson(input);
     	Pessoa pessoa = gson.fromJson(json, Pessoa.class);
-    	FileWriter file = null;
-    	String key_name = "teste.json";
+    	Pessoa pes = new Pessoa();
+    	
+    	pes.persistData(pessoa);
+    	
+    	JSONObject data = (JSONObject)JSONValue.parse(json);
+    	String nomearquivo = (String)data.get("nomearquivo");
+    	
+    	String key_name = nomearquivo;
     	String bucket_name = "marsau-image";
     			
         try {
@@ -34,6 +47,7 @@ public class LambdaFunctionHandler implements RequestHandler<Object, MyLambdaRes
         	System.out.format("Uploading %s to S3 bucket %s...\n", key_name, bucket_name);
         	AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withRegion(Regions.SA_EAST_1).build();
         	s3Client.putObject(bucket_name, key_name, json);
+        	
 		}catch (AmazonServiceException e) {
         	System.err.println(e.getErrorMessage());
         	System.exit(1);
@@ -43,7 +57,7 @@ public class LambdaFunctionHandler implements RequestHandler<Object, MyLambdaRes
         MyLambdaResponse lambdaResponse = new MyLambdaResponse();
         try {
             lambdaResponse.setResponseMessage("Hello " +pessoa.getNome()+" cuja idade é: "+ pessoa.getIdade()
-            +" separando a lista dos guri"+pessoa.getLista());
+            +" separando a lista dos guri"+pessoa.getLista()+" Codigo: `"+pessoa.getId());
             lambdaResponse.setTransactionID(UUID.randomUUID().toString());
         } catch (Exception e) {
             e.printStackTrace();
--- src/main/java/com/amazonaws/lambda/demo/Pessoa.java
+++ src/main/java/com/amazonaws/lambda/demo/Pessoa.java
@@ -3,11 +3,61 @@ package com.amazonaws.lambda.demo;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DeleteItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.GetItemSpec;
+import com.amazonaws.services.dynamodbv2.document.spec.PutItemSpec;
+
 public class Pessoa {
 	
+	long codigo_marcel;
 	String nome;
 	String idade;
 	List<String> lista = new ArrayList<>();
+	DynamoDB dynamoDB;
+	
+	public String DYNAMO_DB_TABLE_NAME = "testeMarcel";
+	
+	private void initDynamoDbClient() {
+		AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().withRegion(Regions.SA_EAST_1).build();
+    	 this.dynamoDB = new DynamoDB(client);	
+		 }
+	
+	//POST
+	public PutItemOutcome persistData(Pessoa pessoa) {
+		initDynamoDbClient();  
+		Table table = dynamoDB.getTable(DYNAMO_DB_TABLE_NAME);
+		  PutItemOutcome outcome = table.putItem(new PutItemSpec().withItem(
+		    new Item().withLong("codigo_marcel", pessoa.getId())
+		               .withString("nome", pessoa.getNome())
+		               .withString("idade", pessoa.getIdade())));
+		  return outcome;
+		 }
+	
+	//GET
+	public String getItem(Pessoa pessoa) {
+		initDynamoDbClient(); 
+		Table table = dynamoDB.getTable(DYNAMO_DB_TABLE_NAME);
+		GetItemSpec spec = new GetItemSpec()
+			    .withPrimaryKey("codigo_marcel", pessoa.getId())
+			    .withProjectionExpression("codigo_marcel, nome, idade")
+			    .withConsistentRead(true);
+		Item item = table.getItem(spec);
+		return item.toJSON();
+	}
+	
+	//DELETE
+	public void deleteItem(Pessoa pessoa) {
+		initDynamoDbClient();
+		Table table = dynamoDB.getTable("DYNAMO_DB_TABLE_NAME");
+		DeleteItemOutcome outcome = table.deleteItem("codigo_marcel", pessoa.getId());
+	}
 	
 	public String getNome() {
 		return nome;
@@ -29,6 +79,15 @@ public class Pessoa {
 	public void setLista(List<String> lista) {
 		this.lista = lista;
 	}
+
+	public long getId() {
+		return codigo_marcel;
+	}
+
+	public void setId(long id) {
+		this.codigo_marcel = id;
+	}
+	
 	
 	
 }
